a1fd9b8ca04c104641a0491b3eef7a4cdabeac46
JustHuangHere <JustHuangHere@gmail.com>
2022-01-13 15:07:49 +0700

    new changes

--- src/main/java/com/justhuanghere/neo/registry/CoffeePlantBlock.java
+++ src/main/java/com/justhuanghere/neo/registry/CoffeePlantBlock.java
@@ -2,15 +2,14 @@ package com.justhuanghere.neo.registry;
 
 import net.minecraft.block.CropBlock;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.Items;
 
 public class CoffeePlantBlock extends CropBlock {
-    protected CoffeePlantBlock(Settings settings) {
+    public CoffeePlantBlock(Settings settings) {
         super(settings);
 
     }
     @Override
     protected ItemConvertible getSeedsItem() {
-        return Items.WHEAT_SEEDS;
+        return NeoItems.COFFEE_BEANS;
     }
 }
